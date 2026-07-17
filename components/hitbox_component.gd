1c948405bab6b3d6a4f6f27edcd4e0f3b5bb1ee7
jchier <chier@pdx.edu>
2026-07-16 21:41:10 -0700

    got rid of signal in hurtbox

--- components/hitbox_component.gd
+++ components/hitbox_component.gd
@@ -3,11 +3,8 @@ extends Area2D
 
 
 signal hit_hurtbox(hurtbox_component: HurtboxComponent)
-signal hit_wall
 var damage: int = 1
 
-func wall_hit():
-	hit_wall.emit
 
 func register_hurtbox_hit(hurtbox_component: HurtboxComponent):
 	hit_hurtbox.emit(hurtbox_component)
