dc7d4a6e6d9d89184cccee8f6fbd7d713c07abf1
jchier <chier@pdx.edu>
2026-08-07 12:47:32 -0700

    queue free the window colliders upon breaking

--- entities/door/door.gd
+++ entities/door/door.gd
@@ -5,12 +5,16 @@ extends Node2D
 @onready var static_body_2d: StaticBody2D = $StaticBody2D
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var static_collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
+@onready var use_l_collision_shape_2d: CollisionShape2D = $Use_L/CollisionShape2D
+@onready var use_r_collision_shape_2d: CollisionShape2D = $Use_R/CollisionShape2D
 
 @export var locked: bool = false
 var open: bool = false
 
 func disable_collision():
 	collision_shape_2d.set_deferred("disabled", true)
+	use_l_collision_shape_2d.set_deferred("disabled", true)
+	use_r_collision_shape_2d.set_deferred("disabled", true)
 	static_collision_shape_2d.set_deferred("disabled", true)
 
 func _on_use_l_body_entered(_body: Node2D) -> void:
--- entities/window/window.gd
+++ entities/window/window.gd
@@ -2,17 +2,15 @@ extends Node2D
 
 const WINDOW_COLLISION_LAYER: int = 20
 const BREAK_FRAME: int = 3
-@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var area_2d: Area2D = $Area2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var static_collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
-@onready var glass_particles: Node2D = $GlassParticles
 var impact_particles_scene: PackedScene = preload("uid://brgnjm0t1n0ag")
+@onready var static_body_2d: StaticBody2D = $StaticBody2D
 
 
 func disable_collision():
-	collision_shape_2d.set_deferred("disabled", true)
-	static_collision_shape_2d.set_deferred("disabled", true)
+	area_2d.queue_free()
+	static_body_2d.queue_free()
 
 
 func _on_area_2d_area_entered(_area: Area2D) -> void:
