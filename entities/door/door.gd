765074c3cc6738e96102118ae9e3a18348d2e02f
jchier <chier@pdx.edu>
2026-08-07 12:39:31 -0700

    doors with colliders

--- entities/door/door.gd
+++ entities/door/door.gd
@@ -2,19 +2,26 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var use_l: Area2D = $Use_L
 @onready var use_r: Area2D = $Use_R
+@onready var static_body_2d: StaticBody2D = $StaticBody2D
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var static_collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
 
 @export var locked: bool = false
 var open: bool = false
 
-
+func disable_collision():
+	collision_shape_2d.set_deferred("disabled", true)
+	static_collision_shape_2d.set_deferred("disabled", true)
 
 func _on_use_l_body_entered(_body: Node2D) -> void:
 	if !open:
 		animation_player.play("open_l")
+		disable_collision()
 		open = true
 
 
 func _on_use_r_body_entered(_body: Node2D) -> void:
 	if !open:
 		animation_player.play("open_r")
+		disable_collision()
 		open = true
