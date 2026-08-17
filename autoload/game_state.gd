a13db413553512d7373f2edc4d3bf906dcd2735d
jchier <chier@pdx.edu>
2026-08-16 21:34:04 -0700

    score thresholds give lives, cash item

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -5,6 +5,12 @@ var docs_remaining: int
 var score: int
 var high_score: int
 
+var score_threshold = {
+	10000: "false",
+	25000: "false",
+	50000: "false",
+	100000: "false"
+}
 func _ready():
 	GameEvent.player_spawned.connect(_on_player_spawned)
 	GameEvent.add_score.connect(_score_changed)
@@ -18,3 +24,8 @@ func _got_document():
 	
 func _score_changed(points: int):
 	score += points
+	print(score_threshold.find_key("false"))
+	if score_threshold and score >= score_threshold.find_key("false"):
+		var passed_score_threshold = score_threshold.find_key("false")
+		score_threshold.erase(passed_score_threshold)
+		player.life_up()
