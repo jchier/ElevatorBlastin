5125e6a21478cd222b3c806714c19533ed53fa50
jchier <156274943+jchier@users.noreply.github.com>
2026-08-17 17:43:40 -0700

    hud displays on pause

--- hud/hud.gd
+++ hud/hud.gd
@@ -55,12 +55,12 @@ func _score_changed(_points: int):
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		heads_up_dialog.visible = !heads_up_dialog.visible
-
 		if !pause_menu.visible:
+			heads_up_dialog.hide()
 			pause_menu.show()
 			get_tree().paused = true
 		else:
+			heads_up_dialog.show()
 			pause_menu.hide()
 			get_tree().paused = false
 		
